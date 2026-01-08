145de840f211bbc0661a9163ef517c9fb500b9dd
arjunkapuluru00-debug <arjunkapuluru00@gmail.com>
2026-01-08 16:16:46 +0530

    Create vowels_consonants_pointers.c

---
+++ vowels_consonants_pointers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    char *ptr;
+    int vowels = 0, consonants = 0;
+  
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+  
+    ptr = str;
+
+    while (*ptr != '\0') {
+        char ch = *ptr;
+        if (ch>='A'&&ch<='Z') {
+            ch = ch + 32;
+        }
+
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+        ptr++;
+    }
+
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+
+    return 0;
+}
